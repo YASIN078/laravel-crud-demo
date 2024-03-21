d8ca5c6dc9af5991c3fec5d20a34538e11d57370
Yasin Mir <0946639@hr.nl>
2024-03-21 15:37:18 +0100

    category added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator; //this line is added to make the pagination look better
 use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Paginator::useBootstrap(); //this line is added to make the pagination look better
         //
     }
 }
